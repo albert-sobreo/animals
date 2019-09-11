346c4c55221e5bc49517c6793db986def04ad30a
Jan Albert Sobreo <janalbertsobreo@icloud.com>
2019-09-11 14:29:32 +0800

    added almost working true of false

--- Assesment3/js/index.js
+++ Assesment3/js/index.js
@@ -1,10 +1,13 @@
 var xseq = [0,1,2,3,4,4,3,2,1,0];
 var yseq = [0,1,0,0,1,0,1,1,0,1];
+var answ = [];
+
+var sum;
 
 var count = 0;
 
 function setWord() {
-  
+    
     // Return random int between min (included) and max (excluded)
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -19,9 +22,6 @@ function setWord() {
       ["Birds Crawl", "Alligators Fly", "Deers Swim", "Rabbits Slither", "Fish Fly"]
     ];
     
-    for(var i = 0; i < 10; i++){
-        console.log(wordList[yseq[i]][xseq[i]]);
-    }
 
     const articles = ['un', 'una'];
     
@@ -35,15 +35,42 @@ function setWord() {
     $word.text(word);
     
     $buttons.click(function() {
+        if(count>=9){
+            for(var i = 0; i<10; i++){
+                sum = answ[i] + sum;
+            };
+            console.log(sum);
+            return;
+        };
         if (this.id == $goodBtn[0].id) {
-            console.log(this.id);
+            console.log("correct");
             count++;
-            $("html").load(location.href+"html>*","");
+            answ.push(1);
+            document.getElementById('test-word').innerHTML = wordList[yseq[count]][xseq[count]];
+            elOrLa = yseq[count];
+            article = articles[elOrLa];
+            $goodBtn = $('#' + article + '-btn');
+            
+            $word = $('#test-word');
+            word = wordList[yseq[count]][xseq[count]];
+            $word.text(word);
             console.log(count);
+            console.log(answ);
             // location.reload(true);
         } else {
-            console.log(this.id);
+            console.log("wrong");
             count++;
+            answ.push(0);
+            document.getElementById('test-word').innerHTML = wordList[yseq[count]][xseq[count]];
+            elOrLa = yseq[count];
+            article = articles[elOrLa];
+            $goodBtn = $('#' + article + '-btn');
+            
+            $word = $('#test-word');
+            word = wordList[yseq[count]][xseq[count]];
+            $word.text(word);
+            console.log(count);
+            console.log(answ);
         }
     });
 }
