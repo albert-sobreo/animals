b7e0916ff0699b04a81a9b592eba4d9076b87b2b
Jan Albert Sobreo <janalbertsobreo@icloud.com>
2019-09-12 16:47:43 +0800

    'hell'

--- Assesment3/js/index.js
+++ Assesment3/js/index.js
@@ -2,7 +2,7 @@ var xseq = [0,1,2,3,4,4,3,2,1,0];
 var yseq = [0,1,0,0,1,0,1,1,0,1];
 var answ = [];
 
-var sum;
+var sum = 0;
 
 var count = 0;
 
@@ -33,19 +33,35 @@ function setWord() {
     var $word = $('#test-word');
     var word = wordList[yseq[count]][xseq[count]];
     $word.text(word);
+    count++;
     
     $buttons.click(function() {
-        if(count>=9){
+        if(count>=11){
             for(var i = 0; i<10; i++){
+                console.log(i);
                 sum = answ[i] + sum;
             };
-            console.log(sum);
+            console.log("sum: "+sum);
+            sum = 0;
             return;
-        };
-        if (this.id == $goodBtn[0].id) {
+        }
+        else if (this.id == $goodBtn[0].id) {
             console.log("correct");
-            count++;
             answ.push(1);
+            if (count == 10){
+                count++;
+                if(count>=11){
+                    for(var i = 0; i<10; i++){
+                        console.log(i);
+                        sum = answ[i] + sum;
+                    };
+                    console.log("sum: "+sum);
+                    sum = 0;
+                    return;
+                }
+                return;
+            }
+            document.getElementById('counter').innerHTML = count + 1;
             document.getElementById('test-word').innerHTML = wordList[yseq[count]][xseq[count]];
             elOrLa = yseq[count];
             article = articles[elOrLa];
@@ -54,13 +70,36 @@ function setWord() {
             $word = $('#test-word');
             word = wordList[yseq[count]][xseq[count]];
             $word.text(word);
+            console.log("sum: "+sum);
             console.log(count);
             console.log(answ);
+            count++;
             // location.reload(true);
         } else {
+            // if(count>=9){
+            //     for(var i = 0; i<10; i++){
+            //         console.log(answ[i]);
+            //         sum = answ[i] + sum;
+            //     };
+            //     console.log("sum: "+sum);
+            //     return;
+            // };
             console.log("wrong");
-            count++;
             answ.push(0);
+            if (count==10){
+                count++;
+                if(count>=11){
+                    for(var i = 0; i<10; i++){
+                        console.log(i);
+                        sum = answ[i] + sum;
+                    };
+                    console.log("sum: "+sum);
+                    sum = 0;
+                    return;
+                }
+                return;
+            }
+            document.getElementById('counter').innerHTML = count + 1;
             document.getElementById('test-word').innerHTML = wordList[yseq[count]][xseq[count]];
             elOrLa = yseq[count];
             article = articles[elOrLa];
@@ -69,8 +108,10 @@ function setWord() {
             $word = $('#test-word');
             word = wordList[yseq[count]][xseq[count]];
             $word.text(word);
+            console.log("sum: "+sum);
             console.log(count);
             console.log(answ);
+            count++;
         }
     });
 }
